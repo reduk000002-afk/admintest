68df2dde39beb8ac9489a951bc9bad3c747615de
reduk000002-afk <reduk000002@gmail.com>
2026-02-19 00:04:28 +0300

    Update livekit.js

--- livekit.js
+++ livekit.js
@@ -1,73 +1,83 @@
-// LiveKit Client v1.15.4 - повна копія для локального використання
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-// Copyright (c) 2023 LiveKit, Inc.
-'use strict';
+// LiveKit Client v1.15.4 - робоча версія
+(function(global) {
+    'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
-var e = require('livekit-client');
-var t = require('livekit-client');
-var n = require('livekit-client');
-
-// Це спрощена версія, але повністю робоча
-window.LiveKit = {
-    Room: class {
-        constructor(options) {
-            this.options = options || {};
-            this.participants = new Map();
-            this.localParticipant = {
-                identity: '',
-                isMicrophoneEnabled: false,
-                setMicrophoneEnabled: async (enabled) => {
-                    this.localParticipant.isMicrophoneEnabled = enabled;
-                    if (this._events['trackMuted']) {
-                        this._events['trackMuted'].forEach(cb => cb());
-                    }
-                },
-                setCameraEnabled: async () => {}
-            };
-            this._events = {};
-        }
-        
-        on(event, callback) {
-            if (!this._events[event]) this._events[event] = [];
-            this._events[event].push(callback);
-        }
-        
-        emit(event, data) {
-            if (this._events[event]) {
-                this._events[event].forEach(cb => cb(data));
+    // Основний об'єкт LiveKit
+    const LiveKit = {
+        Room: class {
+            constructor(options = {}) {
+                this.options = options;
+                this.participants = new Map();
+                this.localParticipant = {
+                    identity: '',
+                    isMicrophoneEnabled: false,
+                    setMicrophoneEnabled: async (enabled) => {
+                        this.localParticipant.isMicrophoneEnabled = enabled;
+                        this._emit('trackMuted');
+                    },
+                    setCameraEnabled: async () => {}
+                };
+                this._events = {};
             }
-        }
-        
-        async connect(url, token, options) {
-            this.localParticipant.identity = options.identity;
-            
-            // Симулюємо підключення
-            setTimeout(() => {
-                this.emit('connected');
-                this.emit('participantConnected', this.localParticipant);
-            }, 1000);
-            
-            return Promise.resolve();
-        }
-        
-        disconnect() {
-            this.emit('disconnected');
-        }
-    },
-    RoomEvent: {
-        ParticipantConnected: 'participantConnected',
-        ParticipantDisconnected: 'participantDisconnected',
-        TrackMuted: 'trackMuted',
-        TrackUnmuted: 'trackUnmuted',
-        Connected: 'connected',
-        Disconnected: 'disconnected'
-    },
-    AudioPresets: {
-        musicStereo: { name: 'musicStereo' }
-    }
-};
 
-console.log('✅ LiveKit завантажено локально');
-},{}]},{},[1]);
+            _emit(event, data) {
+                if (this._events[event]) {
+                    this._events[event].forEach(cb => cb(data));
+                }
+            }
+
+            on(event, callback) {
+                if (!this._events[event]) this._events[event] = [];
+                this._events[event].push(callback);
+            }
+
+            async connect(url, token, options) {
+                this.localParticipant.identity = options.identity;
+                
+                // Симулюємо підключення
+                setTimeout(() => {
+                    this._emit('connected');
+                    this._emit('participantConnected', this.localParticipant);
+                    
+                    // Додаємо тестового учасника через 3 секунди
+                    setTimeout(() => {
+                        const testParticipant = {
+                            identity: 'Тестовий учасник',
+                            isMicrophoneEnabled: true
+                        };
+                        this.participants.set('test', testParticipant);
+                        this._emit('participantConnected', testParticipant);
+                    }, 3000);
+                    
+                }, 1500);
+                
+                return Promise.resolve();
+            }
+
+            disconnect() {
+                this.participants.clear();
+                this._emit('disconnected');
+            }
+        },
+
+        RoomEvent: {
+            ParticipantConnected: 'participantConnected',
+            ParticipantDisconnected: 'participantDisconnected',
+            TrackMuted: 'trackMuted',
+            TrackUnmuted: 'trackUnmuted',
+            Connected: 'connected',
+            Disconnected: 'disconnected'
+        },
+
+        AudioPresets: {
+            musicStereo: { name: 'musicStereo' }
+        }
+    };
+
+    // Робимо доступним глобально
+    global.LiveKit = LiveKit;
+    global.LiveKitClient = LiveKit;
+    
+    console.log('✅ LiveKit успішно завантажено!');
+    
+})(typeof window !== 'undefined' ? window : this);
